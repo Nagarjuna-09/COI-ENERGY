bb191e38ad7bd2ef40d83e20d1f3dab0283cf331
Nagarjuna-09 <nagarjunanathani434@gmail.com>
2023-04-19 12:16:20 -0400

    installed dep. and wrote contracts/id api

--- src/app.js
+++ src/app.js
@@ -7,15 +7,27 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models');
+    const { id } = req.params;
+    const { profile } = req;
+
+    const contract = await Contract.findOne({
+        where: {
+            id,
+            [Op.or]: [
+                { ClientId: profile.id },
+                { ContractorId: profile.id },
+            ],
+        },
+    });
+
+    if (!contract) {
+        return res.status(404).end();
+    }
+    res.json(contract);
+});
+
+
 module.exports = app;
